dfbe7151fa78cff7a2cd18a8e8672f7c3e91e9bb
Kevin Brechbühl <kevin.brechbuehl@unic.com>
2016-03-20 11:42:53 +0100

    Create project with basic initialization processor for dependency injection.

---
+++ src/Foundation/DependencyInjection/code/Pipelines/Initialize/InitializeDependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿namespace Sitecore.Foundation.DependencyInjection.Pipelines.Initialize
+{
+    using Sitecore.Diagnostics;
+    using Sitecore.Pipelines;
+
+    public class InitializeDependencyInjection
+    {
+        public void Process(PipelineArgs args)
+        {
+            Log.Info("Start dependency injection initialization", this);
+        }
+    }
+}
